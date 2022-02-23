1cb7932d3d06b7e3c67ab08896cd5e7afa8fabe2
Traian Stanev <traian.stanev@autodesk.com>
2022-02-23 15:39:58 -0500

    Rename index.js

--- ex1-query-mutate.js
+++ ex1-query-mutate.js
@@ -50,7 +50,7 @@ async function queryElements(modelId, queryDef) {
 
 async function modifyElementProperty(modelId, mutations) {
 
-	const mutateReq = await fetch(`${apiUrl}/modeldata/${foundAsset.modelId}/mutate`, {
+	const mutateReq = await fetch(`${apiUrl}/modeldata/${modelId}/mutate`, {
 		method: 'POST',
 		headers: {
 			...httpOptions.headers,
@@ -240,7 +240,7 @@ async function main() {
 
 
 	//Update a specific property of a specific element.
-	//We will look for Air Handler_v03 : ERV-2 by its serial number (6543214)
+	//We will look for PURY-P240,264,288YSKMU-A : PURY-P240YSKMU-A_460V_Non-Ducted by its serial number 123456
 
 	//First, find the asset
 	let foundAsset;
