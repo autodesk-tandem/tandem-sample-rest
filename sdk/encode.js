ac532b94eca0150d2718f44a4ba1b3fefb4047ab
Traian Stanev <traian.stanev@autodesk.com>
2022-03-03 15:14:16 -0500

    Add copyright notices

--- sdk/encode.js
+++ sdk/encode.js
@@ -1,3 +1,12 @@
+//Copyright 2022 Autodesk, Inc.
+//All rights reserved.
+//
+//This computer source code and related instructions and comments are the
+//unpublished confidential and proprietary information of Autodesk, Inc.
+//and are protected under Federal copyright and state trade secret law.
+//They may not be disclosed to, copied or used by any third party without
+//the prior written consent of Autodesk, Inc.
+//
 
 const { ElementFlags } = require("./dt-schema");
 
