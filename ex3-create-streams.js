890b09966b8cbc583b97c4ddfbf4d8a9cb1e02fd
Traian Stanev <traian.stanev@autodesk.com>
2023-02-23 22:02:42 -0500

    Create levels also

--- ex3-create-streams.js
+++ ex3-create-streams.js
@@ -1,8 +1,6 @@
 import fetch from "node-fetch";
 import config from "config";
-import { AdskAuth } from "./adsk-auth.js";
 import { ColumnFamilies, ColumnNames, ElementFlags, QC } from "./sdk/dt-schema.js";
-import { AttrSchema } from "./sdk/AttrSchema.js";
 import { makeWebsafe, toQualifiedKey } from "./sdk/encode.js";
 import { createElement, getTwinSettings, g_headers, obtainAccessToken, queryElements } from "./sdk/server-query.js";
 import crypto from "crypto"
@@ -12,8 +10,52 @@ const host = config.get("TANDEM_HOST");
 const apiUrl = `https://${host}/api/v1`;
 
 // TODO: Add a specific facility URN to point to (which you can scrape from the browser address bar of a facility loaded into Tandem)
-//Direct link to the facility: https://tandem-stg.autodesk.com/pages/facilities/urn:adsk.dtt:4Y3gKkNgTG-58yX-XmTtNA
 const facilityUrn = "urn:adsk.dtt:7N1I8wsmTX-tV9kjuwvuVQ" 
+
+
+async function createMatchingLevels(defaultModel, levels) {
+
+	let allTandemHosted = await queryElements(defaultModel.modelId, { families: [ColumnFamilies.Standard] }, null, null );
+
+	let tandemHostedLevels = allTandemHosted.filter(row => row[QC.CategoryId]?.[0] === 240);
+
+	let createdCount = 0;
+
+	for (let level of levels) {
+
+		let name = level[QC.Name][0];
+
+		let alreadyHave = tandemHostedLevels.find(row => row[QC.Name][0] === name );
+
+		if (alreadyHave) {
+			continue;
+		}
+
+		let details = {
+			muts: [
+				["i", ColumnFamilies.Standard, ColumnNames.Name, name],
+				["i", ColumnFamilies.Standard, ColumnNames.ElementFlags, ElementFlags.Level],
+				["i", ColumnFamilies.Standard, ColumnNames.CategoryId, 240],
+			]
+		};
+
+		
+		let res = await createElement(defaultModel.modelId, details);
+		console.log(res);
+
+		createdCount ++;
+	}
+
+	if (!createdCount) {
+		return tandemHostedLevels;
+	}
+
+	//Request again to get everything we just created (could be a bit more optimal if we hold on to the response above)
+	allTandemHosted = await queryElements(defaultModel.modelId, { families: [ColumnFamilies.Standard] }, null, null );
+	tandemHostedLevels = allTandemHosted.filter(row => row[QC.CategoryId]?.[0] === 240);
+
+	return tandemHostedLevels;
+}
 
 async function createStreams() {
 
@@ -34,13 +76,26 @@ async function createStreams() {
 	let allElements = await queryElements(mainModel.modelId, { families: [ColumnFamilies.Standard], qualifiedColumns: [QC.Level] }, null, null );
 
 	//Filter by Revit categorys, which for rooms is -2000160 (or just 160 in Tandem)
-	let rooms = allElements.filter(row => row["n:c"]?.[0] === 160);
+	let rooms = allElements.filter(row => row[QC.CategoryId]?.[0] === 160);
+	let levels = allElements.filter(row => row[QC.CategoryId]?.[0] === 240);
+
+	let levelsMap = {};
+	for (let level of levels) {
+		let levelIdBin = Buffer.from(level.k, "base64");
+		let levelIdShort = makeWebsafe(levelIdBin.slice(4).toString("base64"));
+
+		levelsMap[levelIdShort] = level;
+	}
 
 	console.log("Rooms", rooms.length);
 
 	let modelIdBin = Buffer.from(mainModel.modelId.slice(13), "base64");
 
 	let defaultModel = settings.links.find(m => facilityUrn.endsWith(m.modelId.slice(13)));
+
+	
+	let tandemHostedLevels = await createMatchingLevels(defaultModel, levels);
+
 
 	//Create one data connection (element) per room
 	for (let i=0; i<rooms.length; i++) {
@@ -56,10 +111,19 @@ async function createStreams() {
 
 		console.log(sensorQid);
 */
-		let roomName = room["n:n"][0];
+		//Derive stream room reference
+		let roomName = room[QC.Name][0];
 		let roomIdBin = Buffer.from(room.k, "base64");
-
 		let roomXref = makeWebsafe(Buffer.concat([modelIdBin, roomIdBin]).toString("base64"));
+		
+		//Derive stream level reference from room level assignment
+		//This is trickier because the level reference has to be mapped
+		//from the source Revit file to the freshly created Tandem hosted level eleement
+		let roomLevelId = room[QC.Level][0];
+		let roomLevel = levelsMap[roomLevelId];
+		let hostedLevel = tandemHostedLevels.find(l => l[QC.Name][0] == roomLevel[QC.Name][0]);
+		let hostedLevelShortId = makeWebsafe(Buffer.from(hostedLevel.k, "base64").slice(4).toString("base64"));
+
 		
 		let details = {
 			muts: [
@@ -70,24 +134,24 @@ async function createStreams() {
 				["i", ColumnFamilies.Standard, ColumnNames.CategoryId, 8083],
 				["i", ColumnFamilies.Xrefs, ColumnNames.Parent, roomXref],
 				["i", ColumnFamilies.Xrefs, ColumnNames.Rooms, roomXref],
+				["i", ColumnFamilies.Refs, ColumnNames.Level, hostedLevelShortId]
 			]
 		};
 
 		
+		//console.log(details);
 		let res = await createElement(defaultModel.modelId, details);
 		console.log(res);
 	};
 }
 
-function createStreamData(howMany) {
+function createStreamData(startDate, howMany) {
 
 	const yearMinutes = 365 * 24 * 60;
 	const dayMinutes = 24 * 60;
 	const weekMinutes = 7 * dayMinutes;
 	const minuteMs = 60000;
 
-	let nowMs = Date.now();
-	let yearAgo = nowMs - yearMinutes * minuteMs;
 
 	let samples = [];
 	
@@ -117,7 +181,7 @@ function createStreamData(howMany) {
 		let hum = 60 + 20 * Math.sin(2 * Math.PI / dayMinutes * dayMin + rndOffset) * Math.cos(2 * Math.PI / weekMinutes * weekMin);
 
 		let sample = {
-			ts: yearAgo + min * minuteMs,
+			ts: startDate + min * minuteMs,
 			temp,
 			co2,
 			hum
@@ -148,6 +212,8 @@ async function populateStreams() {
 
 	let httpOptions = { headers: g_headers, method: "POST", body: JSON.stringify({keys})};
 
+	//const secretsResetReq = await fetch(`${apiUrl}/models/${defaultModel.modelId}/resetstreamssecrets`, httpOptions);
+
 	const secretsReq = await fetch(`${apiUrl}/models/${defaultModel.modelId}/getstreamssecrets`, httpOptions);
 	
 	const secrets = await secretsReq.json();
@@ -156,17 +222,23 @@ async function populateStreams() {
 
 	let count = 0;
 
+	let startDate = new Date(2022, 1, 1).getTime();
+
 	for (let streamKey in secrets) {
 
 		count++;
 		console.log(count);
 
-		let samples = createStreamData();
+		if (count <= 495) continue;
+
+		let samples = createStreamData(startDate);
+
+		const batch_size = 20000;
 		
-		for (let i=0; i<samples.length; i+= 2000) {
+		for (let i=0; i<samples.length; i+= batch_size) {
 
 			let start = i;
-			let end = Math.min(i+2000, samples.length);
+			let end = Math.min(i+batch_size, samples.length);
 			let data = samples.slice(start, end);
 
 			let post = {
